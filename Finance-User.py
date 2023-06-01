709e4bde869706d078668ccf28d88385ea0c3905
faizack <faizack619@gmail.com>
2023-06-01 06:45:05 -0700

    Added Exception

--- Finance-User.py
+++ Finance-User.py
@@ -72,81 +72,89 @@ def main():
                 st.success("API key submitted:")
 
     if api_key:
-        # Define the cryptocurrency options
-        cryptos = [
-            'BTC-USD', 'ETH-USD', 'XRP-USD', 'BCH-USD', 'LTC-USD', 'DOGE-USD',
-            'USDT-USD', 'ADA-USD', 'BNB-USD', 'LINK-USD', 'XLM-USD', 'SOL1-USD',
-            'THETA-USD', 'ETC-USD', 'FIL-USD', 'TRX-USD', 'EOS-USD', 'XMR-USD',
-            'AVAX-USD'
-        ]
+        try:
+            # Define the cryptocurrency options
+            cryptos = [
+                'BTC-USD', 'ETH-USD', 'XRP-USD', 'BCH-USD', 'LTC-USD', 'DOGE-USD',
+                'USDT-USD', 'ADA-USD', 'BNB-USD', 'LINK-USD', 'XLM-USD', 'SOL1-USD',
+                'THETA-USD', 'ETC-USD', 'FIL-USD', 'TRX-USD', 'EOS-USD', 'XMR-USD',
+                'AVAX-USD'
+            ]
 
-        with st.sidebar.expander("Data Source"):
-            data_source = st.radio("Select Data Source:", ("Online Data", "Offline Data"))
-            offline_data = True if data_source == "Offline Data" else False
+            with st.sidebar.expander("Data Source"):
+                data_source = st.radio("Select Data Source:", ("Online Data", "Offline Data"))
+                offline_data = True if data_source == "Offline Data" else False
 
-        if offline_data:
-            with st.sidebar.expander("Fetech Offline Data"):
-                folder_paths = '/home/developer/faijan/GenCode/gen-Code/Generative-App/Offline-Data/'
-                files = list_files_in_folder(folder_paths)
-                selectedfile = st.selectbox("Select a file", files)
-                st.write("You selected this file:", selectedfile)
-        else:
-            selectedfile = None
-
-        with st.sidebar.expander("Fetch Online Data"):
-            # Get user input
-            crypto = st.selectbox("Select a cryptocurrency", cryptos)
-            start_date = st.date_input("Select a start date")
-            end_date = st.date_input("Select an end date")
-
-            # Add a "Download Data" button
-            if st.button("Download Data"):
-                download_data(crypto, start_date, end_date)
-                st.session_state.show_dataframe = True
-                st.session_state.show_clear = True * 2
-
-        st.session_state.show_clear = True
-
-        if st.session_state.get("show_clear", False):
-            if st.sidebar.button("Clear"):
-                st.session_state.show_dataframe = False
-                st.session_state.show_clear = False
-
-        if st.session_state.show_dataframe:
-            if offline_data and selectedfile:
-                # Display offline data
-                df = pd.read_csv(f"Offline-Data/{selectedfile}")
+            if offline_data:
+                with st.sidebar.expander("Fetech Offline Data"):
+                    folder_paths = '/home/developer/faijan/GenCode/gen-Code/Generative-App/Offline-Data/'
+                    files = list_files_in_folder(folder_paths)
+                    selectedfile = st.selectbox("Select a file", files)
+                    st.write("You selected this file:", selectedfile)
             else:
-                # Display online data
-                df = pd.read_csv(f"Online-Data/{crypto}.csv")
-            st.dataframe(df)
+                selectedfile = None
 
-        with st.form(key='my_form', clear_on_submit=True):
-            prompt = st.text_input("Query:", placeholder="Type your query", key='input')
-            submit_button = st.form_submit_button(label='Send', type='primary')
+            with st.sidebar.expander("Fetch Online Data"):
+                # Get user input
+                crypto = st.selectbox("Select a cryptocurrency", cryptos)
+                start_date = st.date_input("Select a start date")
+                end_date = st.date_input("Select an end date")
 
-        if submit_button and prompt:
-            response = generate_crypto_responses(crypto, start_date, end_date, prompt, api_key,
-                                                 st.session_state.chat_history, offline_data, selectedfile )
+                # Add a "Download Data" button
+                if st.button("Download Data"):
+                    download_data(crypto, start_date, end_date)
+                    st.session_state.show_dataframe = True
+                    st.session_state.show_clear = True * 2
 
-            # Display the question and response
-            st.write(f"Question: {prompt}")
-            st.write(f"Answer: {response}")
+            st.session_state.show_clear = True
 
-        if st.sidebar.button("Show Data"):
-            if offline_data and selectedfile:
-                # Display offline data
-                df = pd.read_csv(f"Offline-Data/{selectedfile}")
-            else:
-                # Display online data
-                df = pd.read_csv(f"Online-Data/{crypto}.csv")
-            st.dataframe(df)
+            if st.session_state.get("show_clear", False):
+                if st.sidebar.button("Clear"):
+                    st.session_state.show_dataframe = False
+                    st.session_state.show_clear = False
 
-        with st.expander("**View Chat History**"):
-            display_chat_history(st.session_state.chat_history)
+            if st.session_state.show_dataframe:
+                if offline_data and selectedfile:
+                    # Display offline data
+                    df = pd.read_csv(f"Offline-Data/{selectedfile}")
+                else:
+                    # Display online data
+                    df = pd.read_csv(f"Online-Data/{crypto}.csv")
+                st.dataframe(df)
 
-        if st.button("Clear Chat History"):
-            st.session_state['chat_history'] = []
+            with st.form(key='my_form', clear_on_submit=True):
+                prompt = st.text_input("Query:", placeholder="Type your query", key='input')
+                submit_button = st.form_submit_button(label='Send', type='primary')
+
+            if submit_button and prompt:
+                response = generate_crypto_responses(crypto, start_date, end_date, prompt, api_key,
+                                                    st.session_state.chat_history, offline_data, selectedfile )
+
+                # Display the question and response
+                st.write(f"Question: {prompt}")
+                st.write(f"Answer: {response}")
+
+                if st.sidebar.button("Show Data"):
+                    try :
+                        if offline_data and selectedfile:
+                            # Display offline data
+                            df = pd.read_csv(f"Offline-Data/{selectedfile}")
+                        else:
+                            # Display online data
+                            df = pd.read_csv(f"Online-Data/{crypto}.csv")
+                        st.dataframe(df)
+                    except :
+                        st.sidebar.warning(f"Please Download {crypto} data")
+
+            with st.expander("**View Chat History**"):
+                display_chat_history(st.session_state.chat_history)
+
+            if st.button("Clear Chat History"):
+                st.session_state['chat_history'] = []
+
+        except AuthenticationError as e :
+            link = "[Click here](https://platform.openai.com/account/api-keys)"
+            st.error(f"Ensure the API key used is correct, clear your browser cache, or generate a new one {link}")
 
     else:
         st.warning("Please add an API key")
